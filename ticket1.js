430162233eb882d4e75013145a5558bbd886eb3c
Nick Goh <166708778+newmaldenite@users.noreply.github.com>
2024-12-02 14:19:33 +0000

    added function convertGBPtocurrency to ticket1.js

--- ticket1.js
+++ ticket1.js
@@ -9,3 +9,17 @@ function convertGBPtoUSD(pounds) { //that takes in a specified amount in £
     return USD 
 }
 
+//Write a function convertGBPToCurrency
+function convertGBPToCurrency(currency, pounds) {
+    if (currency == "EUR") {
+        return convertGBPtoEur(pounds);
+    } 
+    else if (currency == "USD") {
+        return convertGBPtoUSD(pounds);
+    }
+    return "Currency not supported"
+}
+//which takes in both a currency code ("EUR" or "USD") and an amount of £ to convert.
+//It should, based on which code has been entered, 
+// use one of your previously written functions to return the correct conversion. 
+// If an unknown code is entered, it should return null.
