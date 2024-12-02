dcf315ebdb88b7c2a054dd91cf81a186019b3f8a
Nick Goh <166708778+newmaldenite@users.noreply.github.com>
2024-12-02 13:36:13 +0000

    added convertGBPto Eur and convertGBPtoUSD functions to ticket1.js

---
+++ ticket1.js
@@ -0,0 +1,11 @@
+function convertGBPtoEur(pounds) {
+    let euros = pounds * 1.21;
+    return euros
+}
+//Write another function `convertGBPtoUSD` 
+ 
+function convertGBPtoUSD(pounds) { //that takes in a specified amount in £
+    let USD = pounds * 1.27; //converts it to $ using a given exchange rate.
+    return USD 
+}
+
